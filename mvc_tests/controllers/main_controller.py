517970c9d690f3397898acd16883969abeb04de8
reroutedone <zsz948193799@outlook.com>
2025-05-30 17:40:25 +0800

    删除不再需要的目录

--- mvc_tests/controllers/main_controller.py
+++
@@ -1,8 +0,0 @@
-# controllers/main_controller.py
-class MainController:
-    def __init__(self, view):
-        self.view = view
-
-    def handle_text(self, msg):
-        print(f"收到内容：{msg}")
-        # 可以添加逻辑处理，比如保存、验证等
